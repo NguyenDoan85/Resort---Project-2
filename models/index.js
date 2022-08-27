7d97d3b0e6adff9a2f2db17cd1f47fb5373a1fd1
Rhowen01 <rraju002@ucr.edu>
2022-08-27 13:31:07 -0700

    make changes to db file as well as ammending other various others

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const user = require('./user');
 const room = require('./room');
 
-user.hasMany(Room, {
+user.hasMany(room, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
 
-room.belongsTo(User, {
+room.belongsTo(user, {
   foreignKey: 'user_id',
 });
 
-module.exports = { User, Room };
+module.exports = { user, room };
